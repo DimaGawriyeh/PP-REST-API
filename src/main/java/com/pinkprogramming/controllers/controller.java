e4e015c2f49b785cba78ba0eaba2620163bb0545
Gawriyeh <dima.gawriyeh@capgemini.com>
2024-01-07 13:26:37 +0100

    changes in gitignore , added pom and some initail files

---
+++ src/main/java/com/pinkprogramming/controllers/controller.java
@@ -0,0 +1,15 @@
+package com.pinkprogramming.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class controller {
+
+    @GetMapping
+    public String sayHi() {
+        return "Hi";
+    }
+}
