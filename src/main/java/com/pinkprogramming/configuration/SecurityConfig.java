6ff9b4fd1d8cef45548c4236a07e8d04bc40460f
Gawriyeh <dima.gawriyeh@capgemini.com>
2024-01-16 21:42:47 +0100

    Added basic security

---
+++ src/main/java/com/pinkprogramming/configuration/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.pinkprogramming.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails admin = User.withUsername("Admin")
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails user = User.withUsername("User")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(ar ->ar.requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/volunteers/**")).hasRole("ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/com/pinkprogramming/controller/Controller.java
+++ src/main/java/com/pinkprogramming/controller/Controller.java
@@ -4,24 +4,33 @@ import com.pinkprogramming.mapper.Mapper;
 import com.pinkprogramming.request.VolunteerRequest;
 import com.pinkprogramming.service.ApplicationService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
+
 public class Controller {
 
     @Autowired
     private ApplicationService applicationService;
 
 
-    @Operation(summary = "Creates a new voluneer")
+    @Operation(summary = "Creates a new voluneer", security = @SecurityRequirement(name = "basicAuth"))
     @PostMapping("/volunteers")
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<Void> createVolunteer(@Valid @RequestBody VolunteerRequest volunteerRequest) {
         applicationService.createVolunteer(Mapper.mapFromVolunteerRequest(volunteerRequest));
         return ResponseEntity.status(HttpStatus.CREATED).build();
